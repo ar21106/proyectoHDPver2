b0dd65578fac2be45491c6f052bab76bce1e38e7
ar21106 <ar21106@ues.edu.sv>
2023-06-17 15:04:19 -0600

    Revert "Revert "initial commit""
    This reverts commit 6033a8f7aba85e463601192a87fbc2c50e7d989a.

---
+++ ProyectoMVC/ProyectoMVC/Controllers/DescuentosController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Net;
+
+using System.Web.Mvc;
+
+using ProyectoMVC;
+using Rotativa.MVC;
+
+namespace ProyectoMVC.Controllers
+{
+    public class DescuentosController : Controller
+    {
+        private PlanillaEntities db = new PlanillaEntities();
+
+
+        // GET: Descuentos
+        public ActionResult Index()
+        {
+            var descuentos = db.Descuentos.Include(d => d.Empleados);
+            return View(descuentos.ToList());
+        }
+
+        // GET: Descuentos/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Descuentos descuentos = db.Descuentos.Find(id);
+            if (descuentos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(descuentos);
+        }
+
+        
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+       
+        public ActionResult GeneratePDF()
+        {
+            // Lógica adicional para preparar datos, si es necesario
+
+            return new ViewAsPdf("Details")
+            {
+                FileName = "carta.pdf" // Establece el nombre del archivo PDF
+             };
+        }
+    }
+}
