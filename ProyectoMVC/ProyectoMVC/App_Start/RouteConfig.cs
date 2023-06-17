9a2060559044b74e96f5aeaf756306d46628bfc7
ar21106 <ar21106@ues.edu.sv>
2023-06-17 17:40:08 -0600

    views added
    se agregaron las vistas que faltaban

--- ProyectoMVC/ProyectoMVC/App_Start/RouteConfig.cs
+++ ProyectoMVC/ProyectoMVC/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace ProyectoMVC
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Empleados", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
 
             );
 
